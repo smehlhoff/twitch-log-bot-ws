df3c2c4c371608a20317c3f216d2bea73a2608e7
smehlhoff <stevemehlhoff@gmail.com>
2023-06-30 17:51:51 -0700

    Threading now supported

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,16 @@ extern crate serde_derive;
 
 mod lib;
 
-use indicatif::ProgressIterator;
 use lib::{config::Config, db, event::Event, msg::Msg, tags::Tag};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 use tungstenite::{connect, Message};
 
-fn main() {
-    db::create_tables().expect("Error creating postgres table");
-
+fn connect_and_listen(shard_channels: Vec<String>) {
     let v = Arc::new(Mutex::new(Vec::new()));
     let config = Config::load().expect("Error reading config file");
-    let channel_count = config.channels.len();
     let (mut socket, _response) =
         connect(config.server).expect("Error connecting to websocket server");
-
-    println!("Connected to websocket server...");
-    println!("Joining channel(s) now...");
 
     socket
         .write_message(Message::Text(
@@ -36,16 +29,14 @@ fn main() {
     socket.write_message(Message::Text(format!("NICK {}", config.nickname))).unwrap();
 
     // The rate limit to join channels is 50 every 15 seconds.
-    for channel in config.channels.iter().progress() {
-        socket.write_message(Message::Text(format!("JOIN {}", channel))).unwrap();
+    for channel in shard_channels {
+        if !channel.contains("#") {
+            socket.write_message(Message::Text(format!("JOIN #{}", channel))).unwrap();
+        } else {
+            socket.write_message(Message::Text(format!("JOIN {}", channel))).unwrap();
+        }
         thread::sleep(time::Duration::from_millis(320));
     }
-
-    match channel_count {
-        0 => println!("Bot is now logging 0 channels..."),
-        1 => println!("Bot is now logging 1 channel..."),
-        _ => println!("Bot is now logging {} channels...", channel_count),
-    };
 
     loop {
         let data = socket.read_message().expect("Error reading websocket message");
@@ -72,4 +63,48 @@ fn main() {
         }
     }
     // socket.close(None);
+}
+
+fn main() {
+    db::create_tables().expect("Error creating postgres table");
+
+    let config = Config::load().expect("Error reading config file");
+    let channels = config.channels;
+    let channel_count = channels.len();
+
+    match channel_count {
+        0 => println!("Bot is logging 0 channels..."),
+        1 => println!("Bot is logging 1 channel..."),
+        _ => println!("Bot is logging {} channels...", channel_count),
+    };
+
+    let thread_count = {
+        if channel_count < 50 {
+            1
+        } else {
+            let count = channel_count as f64 / 50 as f64;
+            count.ceil() as usize
+        }
+    };
+
+    if thread_count == 1 {
+        connect_and_listen(channels);
+    } else {
+        let chunk_size = channel_count / thread_count;
+        let thread_channels: Vec<Vec<_>> =
+            channels.chunks(chunk_size).map(|c| c.to_vec()).collect();
+        let mut threads = Vec::new();
+
+        for i in 0..=thread_count {
+            let thread_channel_list: Vec<String> =
+                thread_channels[i].iter().map(|x| x.to_owned()).collect();
+            let thread = thread::spawn(move || connect_and_listen(thread_channel_list));
+
+            threads.push(thread);
+        }
+
+        for thread in threads {
+            thread.join().expect("Thread panicked");
+        }
+    }
 }
