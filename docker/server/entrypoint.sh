4d603521d0ed81d0149967ce07bc14a20efec9dd
smehlhoff <stevemehlhoff@gmail.com>
2024-05-28 22:58:28 -0700

    Fix docker files for prod

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -4,7 +4,8 @@ useradd -m $SERVER_USERNAME && \
     echo "$SERVER_USERNAME:$SERVER_PASSWORD" | chpasswd
 
 usermod -aG sudo $SERVER_USERNAME
-
 usermod -s /bin/bash $SERVER_USERNAME
+
+sed -i "s/#Port 22/Port 2222/" /etc/ssh/sshd_config
 
 /usr/sbin/sshd -D
